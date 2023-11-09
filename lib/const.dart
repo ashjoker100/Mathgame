4e2a6f4470c97d235ef07535571ccc1f95b56836
chakhit Saehang <63310054@go.buu.ac.th>
2023-11-09 10:19:19 +0700

    1

---
+++ lib/const.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+var whiteTextStyle = const TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 20,
+  color: Colors.white,
+);
+
+
---
+++ lib/home_page.dart
@@ -0,0 +1,179 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:mathgametutorial/const.dart';
+import 'package:mathgametutorial/util/my_button.dart';
+import 'package:mathgametutorial/util/result_message.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // number pad list
+  List<String> numberPad = [
+    '7',
+    '8',
+    '9',
+    'C',
+    '4',
+    '5',
+    '6',
+    'DEL',
+    '1',
+    '2',
+    '3',
+    '=',
+    '0',
+  ];
+
+  // number A, number B
+  int numberA = 1;
+  int numberB = 1;
+
+  // user answer
+  String userAnswer = '';
+
+  // tapped a button
+  void buttonTapped(String button) {
+    setState(() {
+      if (button == '=') {
+        // calculate correct or incorrect
+        checkResult();
+      } else if (button == 'C') {
+        // clear 
+        userAnswer = '';
+      } else if (button == 'DEL') {
+        // delete 
+        if (userAnswer.isNotEmpty) {
+          userAnswer = userAnswer.substring(0, userAnswer.length - 1);
+        }
+      } else if (userAnswer.length < 3) {
+        userAnswer += button;
+      }
+    });
+  }
+
+  // check  correct or not
+  void checkResult() {
+    if (numberA + numberB == int.parse(userAnswer)) {
+      showDialog(
+          context: context,
+          builder: (context) {
+            return ResultMessage(
+              message: 'Correct!',
+              onTap: goToNextQuestion,
+              icon: Icons.arrow_forward,
+            );
+          });
+    } else {
+      showDialog(
+          context: context,
+          builder: (context) {
+            return ResultMessage(
+              message: 'Sorry try again',
+              onTap: goBackToQuestion,
+              icon: Icons.rotate_left,
+            );
+          });
+    }
+  }
+
+  // create random numbers
+  var randomNumber = Random();
+
+  // GO TO NEXT QUESTION
+  void goToNextQuestion() {
+    // dismiss alert dialog
+    Navigator.of(context).pop();
+
+    // reset values
+    setState(() {
+      userAnswer = '';
+    });
+
+    // create a new question
+    numberA = randomNumber.nextInt(100);
+    numberB = randomNumber.nextInt(100);
+  }
+
+  // GO BACK TO QUESTION
+  void goBackToQuestion() {
+    // dismiss alert dialog
+    Navigator.of(context).pop();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[300],
+      body: Column(
+        children: [
+          // level progress, player needs 5 correct answers in a row to proceed to next level
+          Container(
+            height: 160,
+            color: Colors.deepPurple,
+          ),
+
+          // question
+          Expanded(
+            child: Container(
+              child: Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // question
+                    Text(
+                      numberA.toString() + ' + ' + numberB.toString() + ' = ',
+                      style: whiteTextStyle,
+                    ),
+
+                    // answer box
+                    Container(
+                      height: 20,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple[400],
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                      child: Center(
+                        child: Text(
+                          userAnswer,
+                          style: whiteTextStyle,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+
+          // number pad
+          Expanded(
+            flex: 2,
+            child: Padding(
+              padding: const EdgeInsets.all(2.0),
+              child: GridView.builder(
+                itemCount: numberPad.length,
+                physics: const NeverScrollableScrollPhysics(),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4,
+                ),
+                itemBuilder: (context, index) {
+                  return MyButton(
+                    child: numberPad[index],
+                    onTap: () => buttonTapped(numberPad[index]),
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
